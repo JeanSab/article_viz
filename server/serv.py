46c3e4092cf844facab9ae7b5bb282c70f04e90d
JeanSab <jean.sabourin@free.fr>
2019-01-09 13:45:47 +0100

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   requirements.txt 	new file:   server/serv.py 	new file:   viz/main.html

---
+++ server/serv.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def main_route():
+    print("received call")
+    return 'response from root'
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    print(dict(e))
+    return jsonify(status="error", error_type="400"), 400
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    print(e)
+    return jsonify(status="error", error_type="404"), 404
+
+
+@app.errorhandler(410)
+def internal_error(e):
+    print(e)
+    return jsonify(status="error", error_type="410"), 410
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    print(e)
+    return jsonify(status="error", error_type="500"), 500
