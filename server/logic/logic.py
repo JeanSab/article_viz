d013345d1819f7e09f5e0d6c55fec537c997c84f
JeanSab <jean.sabourin@free.fr>
2019-01-10 22:52:13 +0100

    Changes to be committed: 	modified:   server/logic/logic.py

--- server/logic/logic.py
+++ server/logic/logic.py
@@ -1,13 +1,20 @@
 import json
+import collections
 from datetime import datetime
-import newsOutlet
+from enum import Enum, auto
 
 
 class NewsOutlet():
-
-    def __init__(self, name, url):
-        self.name = name
+    def __init__(self, no, url):
+        self.name = no
         self.url = url
+
+class NewsOutletNames(Enum):
+    GUARDIAN = auto()
+    LEMONDE = auto()
+    BFM = auto()
+    CNEWS = auto()
+    RTL = auto()
 
 
 class Article():
@@ -17,17 +24,35 @@ class Article():
         self.link = link
         self.date = date
         self.newsOutlet = newsOutlet
-
         self.tweets = tweets
 
 
+    def __str__(self):
+        return ("title: " + self.title + "\nlink: " + self.link + "\ndate: " + str(self.date) + "\ntweet count: " + str(len(self.tweets)))
+
+
     @classmethod
-    def loadFromFile(cls, fileLoc):
+    def articleFromFile(cls, fileLoc, nwso):
         with open(fileLoc, "r") as inFile:
             data = json.load(inFile)
 
-        article = cls(data["title"], data["link"], datetime.strptime(data["date"], "%Y-%m-%d"), newsOutlet.newsOutlet(""))
+        article = cls(data["title"], data["link"], datetime.strptime(data["date"], "%Y-%m-%d %H:%M:%S"), nwso)
 
         tweets = {}
-        for t in data["tweets"]
-            tweets[d["id"]] = t
+        for t in data["tweets"]:
+            tweets[t["id"]] = Tweet.fromData(t)
+
+        article.tweets = tweets
+
+        return article
+
+
+class Tweet():
+
+    @classmethod
+    def fromData(cls, data):
+
+        f = collections.namedtuple("Tweet", data.keys())
+        f.__str__ = lambda s: str(s.text)
+
+        return f(*data.values())
